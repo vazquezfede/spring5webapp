7d9f651c4ce3222b5677f8e7c8f70b49ff5a9ede
fvazquez <mariofederico.vazquez.contractor@gdevtools.com>
2024-01-12 12:05:05 -0300

    Prueba commit

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5webappApplication {
-
+//PRUEBA COMMIT
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
